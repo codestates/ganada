40a9b85c30ae7a85e00471f19a0605e62b04ccda
youjung <dbwjdrkddls1@gmail.com>
2022-04-28 16:29:44 +0900

    [clinet]추가: 마이페이지 추가

--- client/src/components/MyPage/Edit.js
+++ client/src/components/MyPage/Edit.js
@@ -1,6 +1,68 @@
-import {} from 'react-router-dom';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { MdPhotoCamera } from 'react-icons/md';
+import { useState } from 'react';
 
 export default function Edit() {
+  // input value change
+  const [inputValue, setInputValue] = useState({
+    nickname: '운영자',
+    phoneNumber: '01012345678',
+  });
+  const [err, setErr] = useState({});
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  // error test
+  const errCheck = (value) => {
+    const errors = {};
+    const { nickname, phoneNumber } = value;
+
+    if (nickname !== '' && !/^[\w\Wㄱ-ㅎㅏ-ㅣ가-힣]{2,20}$/.test(nickname)) {
+      errors.nickname = '최소 2글자이상 특수문자 제외';
+    }
+    if (
+      phoneNumber !== '' &&
+      !/^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})([0-9]{3,4})([0-9]{4})$/.test(
+        phoneNumber,
+      )
+    ) {
+      errors.phoneNumber = '전화번호가 옳바르지 않습니다.';
+    }
+
+    return errors;
+  };
+
+  // 포커스를 빠져나왔을 경우 유효성 검사!
+  const focusBlur = (e) => {
+    setErr(errCheck(inputValue));
+  };
+
+  // 이미지 변경
+  const [imageSrc, setImageSrc] = useState(
+    'https://static.nid.naver.com/images/web/user/default.png?type=s160',
+  );
+  const encodeFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    return new Promise(() => {
+      reader.onload = () => {
+        setImageSrc(reader.result);
+      };
+    });
+  };
+
+  const deleteImg = () => {
+    setImageSrc(
+      'https://static.nid.naver.com/images/web/user/default.png?type=s160',
+    );
+  };
+
   return (
     <div className="mypage-content">
       <div className="inner">
@@ -9,11 +71,24 @@ export default function Edit() {
           <tbody>
             <tr>
               <th>프로필 사진</th>
-              <td>
-                <img
-                  src="https://static.nid.naver.com/images/web/user/default.png?type=s160"
-                  alt=""
+              <td className="profile">
+                <button type="button" onClick={deleteImg}>
+                  사진삭제
+                </button>
+
+                {imageSrc && <img src={imageSrc} alt="preview-img" />}
+
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => {
+                    encodeFile(e.target.files[0]);
+                  }}
                 />
+
+                <label className="icon-wrap" htmlFor="file">
+                  <MdPhotoCamera size="23" />
+                </label>
               </td>
             </tr>
             <tr>
@@ -22,9 +97,37 @@ export default function Edit() {
             </tr>
             <tr>
               <th>닉네임</th>
-              <td>운영자</td>
+              <td>
+                <input
+                  name="nickname"
+                  value={inputValue.nickname}
+                  onChange={handleInput}
+                  onBlur={focusBlur}
+                />
+                <div className="warning">{err.nickname} </div>
+              </td>
+            </tr>
+            <tr>
+              <th>전화번호</th>
+              <td>
+                <input
+                  value={inputValue.phoneNumber}
+                  name="phoneNumber"
+                  onChange={handleInput}
+                  onBlur={focusBlur}
+                />
+                <div className="warning">{err.phoneNumber} </div>
+              </td>
             </tr>
           </tbody>
+          <div className="btn-wrap">
+            <button className={err === null ? 'active' : null} type="submit">
+              취소
+            </button>
+            <button type="submit" className="active">
+              적용
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/MyPage/LeaveId.js
+++ client/src/components/MyPage/LeaveId.js
@@ -1,7 +1,38 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 export default function LeaveId() {
   return (
     <div className="mypage-content">
-      <div className="inner"> 회원탈퇴 inner입니다!</div>
+      <div className="inner">
+        <div className="content-wrap">
+          <div className="content-title">회원탈퇴</div>
+          <div className="leave-wrap">
+            <ul>
+              <div className="leave-title">
+                회원 탈퇴 시 개인정보 및 모든 서비스 이용 기록 삭제됩니다.
+              </div>
+              <li>
+                모든 개인정보 및 개인화 서비스 삭제 / 게시글 삭제 / 채팅 삭제
+              </li>
+              <li>
+                회원 탈퇴 처리 후에는 개인정보를 복원할 수 없으므로 미리 백업해
+                두시길 바랍니다.
+              </li>
+            </ul>
+            <div className="leave-checkbox-wrap">
+              <input type="checkbox" id="leave-check" />
+              <label htmlFor="leave-check">
+                회원탈퇴 유의사항을 모두 확인하였으며 동의합니다.
+              </label>
+            </div>
+          </div>
+          <div className="btn-wrap">
+            <button type="submit">비동의</button>
+            <button className="active" type="submit">
+              탈퇴
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
