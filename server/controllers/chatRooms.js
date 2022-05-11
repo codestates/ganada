ad1c2bf8613c72e93b4cdecf5099a2161fba9a98
cptkuk91 <cptkuk91@gmail.com>
2022-05-12 00:00:31 +0900

    [server] 수정: chatRooms.js

--- server/controllers/chatRooms.js
+++ server/controllers/chatRooms.js
@@ -1,8 +1,14 @@
 const dotenv = require("dotenv");
-const { boards, chatrooms, user_chatroom, chatcontents } = require("../models");
+const {
+  boards,
+  chatrooms,
+  user_chatroom,
+  chatcontents,
+  users,
+} = require("../models");
 const { isAuthorized } = require("./tokenFunctions");
-const users = require("./users");
 dotenv.config();
+const { Op } = require("sequelize");
 
 module.exports = {
   createRoom: async (req, res) => {
@@ -140,15 +146,69 @@ module.exports = {
       if (!userInfo) {
         return res.status(401).json({ message: "권한이 없습니다." });
       } else {
-        const getAllChatRoom = await user_chatroom.findAll({
-          where: { userId: userInfo.id },
-          include: [
-            {
-              model: user_chatroom,
-            },
-          ],
+        const getAllChatRoom = await chatrooms.findAll({
+          where: {
+            [Op.or]: [{ hostId: userInfo.id }, { guestId: userInfo.id }],
+          },
         });
-        return res.status(200).json({ data: getAllChatRoom });
+
+        // const FilterData = getAllChatRoom.data.filter(
+        //   (el) => el.guestId !== userInfo
+        // );
+        // const mapData = FilterData.map((el) => {
+        //   return { ...el, userId: el.guestId };
+        // });
+        // const FilterData2 = getAllChatRoom.data.filter(
+        //   (el) => el.hostId !== userInfo
+        // );
+        // const mapData2 = FilterData2.map((el) => {
+        //   return { ...el, userId: el.hostId };
+        // });
+        // const mapData3 = [...mapData, ...mapData2];
+
+        console.log(getAllChatRoom);
+        return res.status(200).json({ data: { getAllChatRoom } });
+
+        // //여기 보류
+        // // const getAllChatRoom = await user_chatroom.findAll({
+        // //   where: {
+        // //     userId: userInfo.id,
+        // //   },
+        // // });
+        // // const getAllChatRoomId = getAllChatRoom.map((v) => v.chatroomId);
+        // // const findOpponentUser = await chatrooms.findAll({
+        // //   where: {
+        // //     id: {
+        // //       [Op.in]: getAllChatRoomId,
+        // //     },
+        // //   },
+        // // });
+        // // 여기 보류 끝
+
+        // // 접속자가 현재 1번 유저다.
+        // const notHostUser = userInfo.id;
+        // // 2번 유저와 3번 유저랑 채팅한적이 있으면 [16,17,18] --> 2,3
+        // const oppoUser = getAllChatRoom.map((el) => el.id);
+        // const findOpponentUser = await users.findAll({
+        //   where: {
+        //     [Op.not]: [{ id: userInfo.id }],
+        //   },
+        // });
+        // // 접속한 사람이 1일 때 살아 있는 채팅방이면, chatrooms에서 상대방의 userId와 chatroomId가 필요하다.
+        // // 접속한 사람(1번 유저는) 채팅방을 삭제하지 않은 상태다.
+        // // 현재 샘플 데이터로는 [16,2],[17,2], [18,2], [20,3], [21,4], [22,5]
+
+        // // findOpponentUser.hostId 또는 guestId가 userInfo.id와 다른 경우를 뽑으면?
+        // // 1번 유저가 포함 된 상대방의 정보를 가져와야 한다.
+        // const chatList = await chatrooms.findAll({
+        //   where: {
+        //     id: oppoUser,
+        //   },
+        // });
+        // const chatListMap = chatList.map((el) => el.dataValues);
+        // console.log(oppoUser);
+        // 접속자가 hostId에 포함 된 경우 guestId 상대방을 찾고,
+        // 접속자가 guestId에 포함 된 경우 hostId를 찾아라.
       }
     } catch (err) {
       console.log(err);
