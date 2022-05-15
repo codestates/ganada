85451efd2d3db95507c300674a4effbf58188647
cptkuk91 <cptkuk91@gmail.com>
2022-05-15 22:10:18 +0900

    [client/server] 수정: signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -102,7 +102,7 @@ export default function Signup({ setModal }) {
       };
       try {
         await axios
-          .post(`${process.env.REACT_APP_API_URL}auth/signup`, data, {
+          .post(`${process.env.REACT_APP_API_URL}/auth/signup`, data, {
             withCredentials: true,
           })
           .then((res) => {
@@ -133,7 +133,7 @@ export default function Signup({ setModal }) {
     } else {
       try {
         await axios
-          .post(`${process.env.REACT_APP_API_URL}auth/mailVerification`, {
+          .post(`${process.env.REACT_APP_API_URL}/auth/mailVerification`, {
             email: inputValue.email,
           })
           .then((res) => {
